e23dd5fd7137b12b3731db022d5b1aad176da872
DaltonHart <daltonvaren@gmail.com>
2018-06-29 12:45:48 -0700

    created landing carousel

--- client/src/components/welcome.js
+++ client/src/components/welcome.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Carousel from 'nuka-carousel';
 
 export default () => {
   return (
-    <div>
-      This is the landing page...
-      sign in to access the secure page
+    <div className="landing">
+      <div className="carousel-container">
+      <Carousel>
+        <img src="./Golden-Gate-(San-Francisco).jpg" />
+        <img src="./new-york-10.jpg" />
+        <img src="./tokyo-4k-wallpaper-103.jpg" />
+        <img src="./paris.jpg" />
+      </Carousel>
+      </div>
     </div>
   )
 }
