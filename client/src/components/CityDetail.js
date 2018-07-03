c295b410ae9f2c1ecd74befa11a7f0145bdc763c
juney3 <juney37@gmail.com>
2018-07-03 12:22:41 -0700

    added post components

--- client/src/components/CityDetail.js
+++ client/src/components/CityDetail.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 import RaisedButton from 'material-ui/RaisedButton'
 import Modal from 'react-responsive-modal'
 import PostForm from './auth/post_form'
+import PostList from './PostList'
 
 
 class CityDetail extends Component{
@@ -27,11 +28,14 @@ class CityDetail extends Component{
     const { open } =this.state;
     return(
       <div>
+        <h4>City Name</h4>
         <p>I am the city detail page!</p>
+
         <RaisedButton type="" onClick={this.onOpenModal} label="Add Post" primary={true} labelColor={'#FFFFFF'}/>
           <Modal open={open} onClose={this.onCloseModal}>
             <PostForm/>
           </Modal>
+          <PostList/>
       </div>
     )
   }
